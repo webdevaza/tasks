25a941f87a317eea30f0d77aafdd3c461e83725d
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-01-24 19:05:40 +0600

    Azajan24 (#2)
    * wrote a plan in readme2
    
    * modified views: changed welcome page, modified dashboard
    
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('All tasks') }}
                     </x-nav-link>
                 </div>
             </div>
