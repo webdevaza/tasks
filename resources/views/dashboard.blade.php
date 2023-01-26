671714fb86f185af5023ef25272aa1894234ebda
Azamat <azamailnet@mail.ru>
2023-01-26 22:00:24 +0600

    tried to use js, but then i changed my mind

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
         <div class="container mx-auto">
             <form action="" method="post">
                 <div class="flex justify-center">
-                    <input class="w-full bg-gray-100 text-gray-700 rounded h-14 border-double border-4 border-black" type="text" value="" placeholder="Type your task here.">
+                    <input name="new_task" class="w-full bg-gray-100 text-gray-700 rounded h-14 border-double border-4 border-black" type="text" value="" placeholder="Type your task here.">
                 </div>
                 <div class="flex justify-center m-4">
                     <button class="bg-amber-300 px-4 py-2 rounded font-bold text-green-900">Add</button>
@@ -14,7 +14,7 @@
     </x-slot>
     <div class="container mx-auto pb-2">
         <x-task-update />
-        <x-task task="Birinch jumush" check=""/>
+        <x-task task="Birinchi jumush" check=""/>
         <x-task task="Ekinchi jumush" check=""/>
         <x-task task="Uchunchu jumush" check=""/>
         <x-task task="dfdf jumush" check=""/>
