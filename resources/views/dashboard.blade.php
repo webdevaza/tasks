f08b52ba89dde49459492c4911af9db1a75c05e6
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-01-26 16:00:15 +0600

    aza added task and task-update components (#5)
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
+
     <x-slot name="header">
-        <div class="container">
+        <div class="container mx-auto">
             <form action="" method="post">
                 <div class="flex justify-center">
                     <input class="w-full bg-gray-100 text-gray-700 rounded h-14 border-double border-4 border-black" type="text" value="" placeholder="Type your task here.">
@@ -11,60 +12,18 @@
             </form>
         </div>
     </x-slot>
-    <div class="container mx-auto">
-        <div class="flex flex-row m-4">
-            <div class="flex gap-2 w-full">
-                <div class="flex justify-center items-center mx-2" >
-                    <input class="justify-center w-6 h-6" type="checkbox">
-                </div>
-                <div class="container text-gray-900 mx-auto">
-                    <div class="flex w-full rounded bg-gray-100 ">
-                        <p class="p-2">kjnkjn kjsndkjn kjnkjncks lsdnlkdcn</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-update-logo/>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-delete-logo/>
-            </div>
-        </div>
-        <div class="flex flex-row m-4">
-            <div class="flex gap-2 w-full">
-                <div class="flex justify-center items-center mx-2" >
-                    <input class="justify-center w-6 h-6" type="checkbox">
-                </div>
-                <div class="container text-gray-900 mx-auto">
-                    <div class="flex w-full rounded bg-gray-100 ">
-                        <p class="p-2">kjnkjn kjsndkjn kjnkjncks lsdnlkdcn</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-update-logo/>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-delete-logo/>
-            </div>
-        </div>
-        <div class="flex flex-row m-4">
-            <div class="flex gap-2 w-full">
-                <div class="flex justify-center items-center mx-2" >
-                    <input class="justify-center w-6 h-6" type="checkbox" checked>
-                </div>
-                <div class="container text-gray-500 mx-auto line-through">
-                    <div class="flex w-full rounded bg-gray-100 ">
-                        <p class="p-2 ">kjnkjn kjsndkjn kjnkjncks lsdnlkdcn</p>
-                    </div>
-                </div>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-update-logo/>
-            </div>
-            <div class="flex mt-1 mb-2 mx-2">
-                <x-delete-logo/>
-            </div>
-        </div>
+    <div class="container mx-auto pb-2">
+        <x-task-update />
+        <x-task task="Birinch jumush" check=""/>
+        <x-task task="Ekinchi jumush" check=""/>
+        <x-task task="Uchunchu jumush" check=""/>
+        <x-task task="dfdf jumush" check=""/>
+        <x-task task="dfvdfv jumush" check=""/>
+        <x-task task="ghnghn jumush" check=""/>
+        <x-task task="hj,jk, jumush" check="checked" />
+        <x-task task="ghjumy jumush" check="checked" />
+        <x-task task="o;io jumush" check="checked" />
+        <x-task task="Tortyujuyujunchu jumush" check="checked" />
+        
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="main.js"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-green-600">
