f0769bad23de33b3591a649b79ccd76a7a93fb58
webdevaza <119423737+webdevaza@users.noreply.github.com>
2023-02-07 14:51:19 +0600

    removed ajax functions (#18)
    Co-authored-by: Azamat <azamailnet@mail.ru>

--- public/ajax.js
+++ public/ajax.js
@@ -1,17 +1,17 @@
-$(document).ready(function () {
-    $(".task-check").change(function () {
-        var checkbox = $(this);
-        var taskId = checkbox.attr("id").split("-")[1];
-        var route = checkbox.is(":checked") ? 'tasks.fulfil' : 'tasks.unFulfil';
+// $(document).ready(function () {
+//     $(".task-check").change(function () {
+//         var checkbox = $(this);
+//         var taskId = checkbox.attr("id").split("-")[1];
+//         var route = checkbox.is(":checked") ? 'tasks.fulfil' : 'tasks.unFulfil';
 
-        console.log('jquery is working')
-        $.ajax({
-            type: "POST",
-            url: "/ajax/runRoute",
-            data: { _token: "{{ csrf_token() }}", route: route, taskId: taskId },
-            success: function (data) {
-                console.log(data);
-            }
-        });
-    });
-});
+//         console.log('jquery is working')
+//         $.ajax({
+//             type: "POST",
+//             url: "/ajax/runRoute",
+//             data: { _token: "{{ csrf_token() }}", route: route, taskId: taskId },
+//             success: function (data) {
+//                 console.log(data);
+//             }
+//         });
+//     });
+// });
